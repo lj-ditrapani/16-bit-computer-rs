df1729f2c6a6f555c92dddd978b2fcf0e4c62ab5
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2026-01-17 18:50:53 -0500

    fix(computer): bubble up errors from cpu.step
    through computer.execute_frame

--- src/computer.rs
+++ src/computer.rs
@@ -20,8 +20,8 @@ impl Computer {
         const INSTRUCTIONS_PER_FRAME: u32 = 34_440;
 
         for _ in 0..INSTRUCTIONS_PER_FRAME {
-            let instruction_result = self.cpu.step(&mut self.memory);
-            if matches!(instruction_result, Ok(InstructionResult::Halt)) {
+            let instruction_result = self.cpu.step(&mut self.memory)?;
+            if matches!(instruction_result, InstructionResult::Halt) {
                 return Ok(());
             }
         }
